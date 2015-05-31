b570d35b5e353ce04ff534a5e444ce1ab292ab46
blop <blopblopy@users.noreply.github.com>
2015-05-31 21:16:58 +0300

    excel to word for rasham

--- rasham/e2w.py
+++ rasham/e2w.py
@@ -5,6 +5,11 @@ from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.text.run import Font
 from docx.enum.style import WD_STYLE_TYPE
 from docx.shared import Pt
+import csv
+from collections import OrderedDict
+
+csv.dict = OrderedDict
+reader = csv.DictReader(open("data.csv"))
 
 document = docx.Document()
 style = document.styles.add_style("hebrew", WD_STYLE_TYPE.PARAGRAPH)
@@ -15,18 +20,13 @@ font.name = "Arial Hebrew"
 font.rtl = True 
 font.size = Pt(12)
 
-
-techen = u"שלום"
-tichnun = u"סופ"
-
-p = document.add_paragraph(style="hebrew")
-r = p.add_run(u"תחום התכן:")
-r.font.cs_bold = True
-r = p.add_run(u" " + techen)
-
-p = document.add_paragraph(style="hebrew")
-r = p.add_run(u"שלב תכנון:")
-r.font.cs_bold = True
-r = p.add_run(u" " + tichnun)
+for line in reader:
+    i = line.iteritems()
+    for k,v in i:
+        p = document.add_paragraph(style="hebrew")
+        r = p.add_run(k.decode("utf8") + ": ")
+        r.font.cs_bold = True
+        r = p.add_run(v.decode("utf8"))
+    document.add_page_break()
 
 document.save("pytest.docx")
