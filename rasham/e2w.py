cc114ee3bb283affb5a91f029dcd548774c1a15c
blop <blopblopy@users.noreply.github.com>
2015-06-15 22:19:56 +0300

    some more changes to excel to word

--- rasham/e2w.py
+++ rasham/e2w.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import sys, time
 import docx
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.text.run import Font
 from docx.enum.style import WD_STYLE_TYPE
 from docx.shared import Pt
+
 import csv
 from collections import OrderedDict
 
 csv.dict = OrderedDict
-reader = csv.DictReader(open("data.csv"))
+reader = csv.DictReader(open(sys.argv[1]))
 
 document = docx.Document()
 style = document.styles.add_style("hebrew", WD_STYLE_TYPE.PARAGRAPH)
 style.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.RIGHT
 font = style.font
 font.complex_script = True
-font.name = "Arial Hebrew"
+font.name = "David"
 font.rtl = True 
 font.size = Pt(12)
 
@@ -26,7 +29,9 @@ for line in reader:
         p = document.add_paragraph(style="hebrew")
         r = p.add_run(k.decode("utf8") + ": ")
         r.font.cs_bold = True
+        r.font.rtl = True
         r = p.add_run(v.decode("utf8"))
+        r.font.rtl = True
     document.add_page_break()
 
-document.save("pytest.docx")
+document.save("rasham_%s.docx" % time.strftime("%Y%m%d_%H%M%S"))
