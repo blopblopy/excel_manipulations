6bc45f37bc198cc55b983fdb9bacc45a46332552
blop <blopblopy@users.noreply.github.com>
2015-06-23 07:06:40 +0300

    adding graphs of 3 dimensions

--- pca/pca_first.py
+++ pca/pca_first.py
@@ -2,8 +2,28 @@ import csv
 import numpy as np
 from sklearn.decomposition import PCA
 
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import random
 
+
+def draw(men, women, filename):
+    fig = plt.figure(filename)
+    ax = fig.add_subplot(111, projection='3d')
+    for man_x,man_y,man_z in men:
+        ax.scatter(man_x, man_y, man_z, c="g", marker="^")
+    for woman_x,woman_y,woman_z in women:
+        ax.scatter(woman_x, woman_y, woman_z, c="r", marker="o")
         
+
+
+def do_draw(men, women, filename):
+    pca = PCA(3)
+    together = []
+    together.extend(men)
+    together.extend(women)
+    pca.fit(together)
+    draw(pca.transform(men), pca.transform(women), filename)
 
 def do_pca(pca, data):
     pca.fit(data)
@@ -17,6 +37,7 @@ def do_pcas(data, label):
     half_pca = PCA(0.75)
     do_pca(half_pca, data)
     print ("======= END %s ==========" % label)
+    return half_pca
     
 
 def score_against(base, other):
@@ -46,6 +67,31 @@ def parse_file(filename):
     men_data = np.array(men)
     return together, women, men
 
+
+def random_data(filename, together):
+    data_high = []
+    data_low = []
+    for element in np.transpose(together):
+        so = sorted(element)
+        high = so[:len(so)/2]
+        low = so[len(so)/2:]
+        random.shuffle(high)
+        random.shuffle(low)
+        data_high.append(high)
+        data_low.append(low)
+
+    data_high = np.transpose(data_high)
+    data_low = np.transpose(data_low)
+
+    print "RANDOM"
+    print(score_against(data_high, data_low))
+    print(score_against(data_low, data_high))
+    do_draw(data_high, data_low, filename + " - RANDOM")
+    
+    
+    
+
+
 def do_file(filename):
     print "============ %s ===========" % filename
 
@@ -55,12 +101,19 @@ def do_file(filename):
     do_pcas(women, "WOMEN")
     do_pcas(men, "MEN")
 
+    do_draw(men, women, filename)
+
+    random_data(filename, together)
+    
+
     m_on_f = score_against(women, men)
     print m_on_f
     f_on_m = score_against(men, women)
     print f_on_m
     m_on_m = score_against(men, men)
     print m_on_m
+    f_on_f = score_against(women, women)
+    print f_on_f
     
     print
     print
@@ -69,9 +122,17 @@ def do_file(filename):
 
 do_file("israeli_brain.csv")
 do_file("VBM.csv")
+plt.show()
 # create f on f, m on m
 # graph 3 components with colors
 # create seperate groups using minimum/maximum data.
 # understand what the components are
+
+# PCA!
+# understand what the score_samples calculates
+# check the number of men vs women that fit certain thresholds of scoring.
+# check jonathan's svd
+# read about GMM - and some basic runs.   
+
 
 
